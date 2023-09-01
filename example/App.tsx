cc19893a6fdbe8152ccf3c7584f2973c35a61667
arekm213 <Arqm213@gmail.com>
2023-09-01 14:24:35 +0200

    Updated example

--- example/App.tsx
+++ example/App.tsx
@@ -1,13 +1,21 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import {useEffect} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Platform } from 'react-native';
+import {useEffect, useState} from 'react';
 import Slider from '@react-native-community/slider';
 
 import * as Torch from 'expo-torch';
 
 export default function App() {
-  const torchStrengthArray = [0,1,2,3,4,5]
+  const [torchLevelsArray, setTorchLevelsArray] = useState<number[]>([]);
 
-  const showTorchLevels = async() => {
+  const initAndroidTorchLevelsArray = async() => {
+    const torchMaxStrength = await Torch.getMaxLevel();
+    const levelsArray = [...Array(torchMaxStrength)].map((item, index) => {
+      return index + 1;
+    });
+    setTorchLevelsArray(levelsArray);
+  }
+
+  const logTorchLevels = async() => {
     const torchMaxStrength = await Torch.getMaxLevel();
     const torchStrength = await Torch.getCurrentLevel();
     const defaultTorchStrength = await Torch.getDefaultLevel();
@@ -23,23 +31,36 @@ export default function App() {
   }
 
   useEffect(() => {
-    showTorchLevels();
+    logTorchLevels();
+    if(Platform.OS === 'android'){
+      initAndroidTorchLevelsArray();
+    }
   }, [])
 
   return (
     <View style={styles.container}>
       <Text></Text>
-      <TouchableOpacity style={[styles.rect, { backgroundColor:'green' }]} onPress={turnOn} />
-      <TouchableOpacity style={[styles.rect, { backgroundColor:'red' }]} onPress={turnOff} />
-      <TouchableOpacity style={[styles.rect, { backgroundColor:'blue' }]} onPress={showTorchLevels} />
-      <View style={{flexDirection: 'row'}}>
-        {torchStrengthArray.map((val) => {
-          return <TouchableOpacity style={[styles.rect, { backgroundColor:'yellow' }]} onPress={() => Torch.turnOnWithLevel(val)}>
-            <Text style={{fontSize: 50}}>{val}</Text>
+      <TouchableOpacity style={styles.rect} onPress={Torch.toggle}>
+        <Text>Toggle</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.rect} onPress={turnOn}>
+        <Text>Turn on</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.rect} onPress={turnOff}>
+        <Text>Turn off</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.rect} onPress={logTorchLevels}>
+        <Text>Log torch levels</Text>
+      </TouchableOpacity>
+      {Platform.OS === 'android'
+      ? <View style={{flexDirection: 'row'}}>
+        {torchLevelsArray.map((val) => {
+          return <TouchableOpacity style={[styles.torchLevelRect, { backgroundColor:'yellow' }]} onPress={() => Torch.turnOnWithLevel(val)}>
+            <Text style={{fontSize: 30}}>{val}</Text>
           </TouchableOpacity>
         })}
       </View>
-      <Slider
+      : <Slider
         style={{width: 200, height: 40}}
         minimumValue={0}
         maximumValue={1}
@@ -49,7 +70,7 @@ export default function App() {
         }}
         minimumTrackTintColor="#00FF00"
         maximumTrackTintColor="#000000"
-      />
+      />}
     </View>
   );
 }
@@ -60,6 +81,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    paddingTop: 40,
+    alignItems:'center'
   },
-  rect: { width: 100, height: 100 }
+  rect: { paddingVertical: 10, paddingHorizontal: 20, borderWidth: 1, alignItems:'center', margin: 10 },
+  torchLevelRect: {
+    paddingVertical: 5, paddingHorizontal: 5, borderWidth: 1, alignItems:'center', margin: 5
+  }
 });
